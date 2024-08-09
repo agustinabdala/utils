e0743bedebadfd14d550fbfc0ad932569cd314fd
Agustin Abdala <agu2@hotmail.com>
2024-08-09 10:01:05 -0300

    Actualizar read_msproject.py

--- read_msproject.py
+++ read_msproject.py
@@ -4,16 +4,28 @@ import mpxj
 jpype.startJVM()
 print(jpype.getJVMVersion())
 from net.sf.mpxj.reader import UniversalProjectReader
-project = UniversalProjectReader().read('path_to_your_project_file.mpp')
+project = UniversalProjectReader().read('240226- Plan AV 1039 IDBP 70.mpp')
 
 print("Tasks")
+
 for task in project.getTasks():
-	print(f"ID: {task.getID()},\n\
-	Name: {task.getName()},\n\
+    print(f"ID: {task.getID()},\n\
+        Name: {task.getName()},\n\
         Start: {task.getStart()},\n\
         Finish: {task.getFinish()},\n\
         Duration: {task.getDuration()},\n\
-        ResourceNames: {task.getResourceNames()}\n\
-		Percentage: {task.getPercentageComplete()}\
-		")
+        ResourceNames: {task.getResourceNames()},\n\
+        PercentageComplete: {task.getPercentageComplete()},\n\
+        Notes: {task.getNotes()},\n\
+        Priority: {task.getPriority()},\n\
+        BaselineStart: {task.getBaselineStart()},\n\
+        BaselineFinish: {task.getBaselineFinish()},\n\
+        ActualStart: {task.getActualStart()},\n\
+        ActualFinish: {task.getActualFinish()},\n\
+        Work: {task.getWork()},\n\
+        ActualWork: {task.getActualWork()},\n\
+        RemainingWork: {task.getRemainingWork()},\n\
+        Milestone: {task.getMilestone()},\n\
+        LevelingDelay: {task.getLevelingDelay()}\n\
+        ")
 jpype.shutdownJVM()
